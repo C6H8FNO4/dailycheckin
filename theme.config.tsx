025f577eb7ba79be8b9f34ade20c4cc7ef2dd8f0
Sitoi <133397418@qq.com>
2024-02-06 15:02:51 +0800

    🌐 更新(package-lock.json)：更新 package-lock.json 更新配置文件   ✨ 功能(pages/history/2024-02-06.mdx)：新增「奥拉星」签到   💄 优化(pages/history/2024-02-06.mdx)：优化「什么值得买」日志输出   🚀 部署(theme.config.tsx)：更新 banner 的配置   📝 文档(pages/history/_meta.json)：增加 2024-02-06 的文档元数据

--- theme.config.tsx
+++ theme.config.tsx
@@ -63,13 +63,13 @@ const config: DocsThemeConfig = {
     }
   },
   banner: {
-    key: '2024.1.20-release',
+    key: '2024.2.6-release',
     text: (
       <a
-        href="/dailycheckin/history/2024-01-20/"
+        href="/dailycheckin/history/2024-02-06/"
         target="_blank"
       >
-        🎉 DailyCheckIn 2024.1.20 is released. Read more →
+        🎉 DailyCheckIn 2024.2.6 is released. Read more →
       </a>
     ),
   },
